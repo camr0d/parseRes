c30916ba6985e20fb7148722cba0c8ad851d226d
camr0d <68032209+camr0d@users.noreply.github.com>
2023-07-29 14:48:45 +0300

    Add support for counting by any number of posts telegram

--- main.py
+++ main.py
@@ -10,10 +10,15 @@ class HomeScreen(QtWidgets.QMainWindow):
         super(HomeScreen, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        #self.app = Client("my_account", api_id, api_hash)
+        self.maxMessageRequest = 10
+        
+        # Инициализация подключения к telegram
         self.app = Client("my_account")
+        
+        # Инициализация действий кнопок
         self.ui.pushButton.clicked.connect(self.calculate)
         
+    # Подсчет количества просмотров и участников указанного сообщества
     def calculate(self) -> None:
         average_view = 0
         countMembers = 0
@@ -22,8 +27,16 @@ class HomeScreen(QtWidgets.QMainWindow):
         count_elements = int(self.ui.le_countMessage.text())
         with self.app:
             countMembers = self.app.get_chat(chat_id).members_count
-            for message in self.app.get_chat_history(chat_id, limit=count_elements):
-               average_view += message.views
+            for message in self.app.get_chat_history(chat_id, limit=1):
+                maxMessage = message.id
+            if (count_elements > maxMessage):
+                count_elements = maxMessage - 1 
+            for x in range(int(count_elements / self.maxMessageRequest)):
+                for message in self.app.get_chat_history(chat_id, limit=self.maxMessageRequest, offset=x * self.maxMessageRequest):
+                    average_view += message.views
+            if (count_elements % self.maxMessageRequest):
+                for message in self.app.get_chat_history(chat_id, limit=count_elements % self.maxMessageRequest, offset=int(count_elements / self.maxMessageRequest) * self.maxMessageRequest):
+                    average_view += message.views
         average_view = int(average_view / count_elements)
         self.ui.le_aberageView.setText(str(average_view))
         self.ui.le_countUser.setText(str(countMembers))
